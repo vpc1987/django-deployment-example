3c784f4671f35987971884f239c6d14a18dab35b
vishnuprasad1987 <you@example.com>
2020-06-27 06:36:19 -0400

    first

---
+++ learning_users/basic_app/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+
+from basic_app.models import UserprofileInfo
+
+# Register your models here.
+
+admin.site.register(UserprofileInfo)
---
+++ learning_users/basic_app/forms.py
@@ -0,0 +1,22 @@
+from django import forms
+
+from django.contrib.auth.models import User
+
+from basic_app.models import UserprofileInfo
+
+class UserForm(forms.ModelForm):
+
+	password = forms.CharField(widget=forms.PasswordInput())
+
+	class Meta:
+		model = User
+		fields = ('username', 'email', 'password')
+
+class UserProfileInfoForm(forms.ModelForm):
+	
+	class Meta:
+		model = UserprofileInfo
+		fields = ('protfolio_site','profile_pic')
+
+		
+			
